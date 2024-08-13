640bf721b91656439b727bf2280539a131b04b34
PRAKASHGOWDA RUDRAGOUDA HOSAMANI <124488941+prakashgowda18@users.noreply.github.com>
2024-08-13 13:30:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// var random1=Math.floor(Math.random()*6)+1;
+// var randomDiceImage="dice"+random1+".png";
+// var imageRandomSource="images/"+randomDiceImage;
+// document.querySelectorAll("img").setAttribute("src",imageRandomSource);
+alert("Hello");
